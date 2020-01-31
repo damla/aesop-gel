a8b2347779c318f836a4ab0f2e6ab6737d36666c
Beau Charman <beau.charman@aesop.com>
2020-01-31 15:53:03 +1100

    testing

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,22 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
 module.exports = {
-  // setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  roots: ['src'],
+  moduleDirectories: ['node_modules', 'src'],
+  unmockedModulePathPatterns: ['react'],
+  testURL: 'http://localhost/',
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 40,
-      functions: 40,
-      lines: 40,
-      statements: 40,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
-  // transform: {
-  //   '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
-  //   '^.+\\.tsx?$': 'ts-jest',
-  // },
-  roots: ['src'],
-  moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
-    'src/(.*)': '<rootDir>/src/$1',
-    '^~(.*)$': '<rootDir>/src$1',
-  },
-  moduleDirectories: ['node_modules', 'src'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: [`<rootDir>/(dist|docs|node_modules|.cache)/`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
-    __PATH_PREFIX__: ``,
     __TEST__: true,
   },
-  testURL: `http://localhost`,
-  // setupFiles: [`<rootDir>/loadershim.js`],
-  unmockedModulePathPatterns: ['react'],
+  transform: {
+    '^.+\\.js?$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['<rootDir>/(dist|docs|node_modules)/'],
 };
