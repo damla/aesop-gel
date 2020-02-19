d6f89212e3e882346bd737000811d45a9f790382
Beau Charman <beau.charman@gmail.com>
2020-02-19 16:12:08 +1100

    add kit and lists

--- pipeline/KitList/KitList.prop-types.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const PROP_TYPES = {
-  className: PropTypes.string,
-  items: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
-
-export default PROP_TYPES;
--- src/components/KitList/KitList.stories.js
+++ src/components/KitList/KitList.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import 'normalize.css';
-
 import '~/styles/base.module.css';
 import '~.storybook/storybook.module.css';
 import KitList from './KitList';
