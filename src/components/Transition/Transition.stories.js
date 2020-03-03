3a3eab930f7910ef592903c726fc0a30d09646a8
Beau Charman <beau.charman@gmail.com>
2020-03-04 09:06:05 +1100

    story for Transition

--- src/components/Transition/Transition.stories.js
+++ src/components/Transition/Transition.stories.js
@@ -1,28 +1,34 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
+import Button from '~/components/Button';
 import DefinitionList from '~/components/DefinitionList';
 import Transition from './Transition';
 
 storiesOf('Transition', module).add('Base component', () => {
+  /* eslint-disable react/prop-types */
+  /** @TODO Make a HOC from State*/
   const State = ({ children }) => {
     const [state, setState] = React.useState(true);
 
     return <div>{children(state, setState)}</div>;
   };
+  /* eslint-enable react/prop-types */
 
   return (
     <State>
       {(state, setState) => (
         <>
-          <button
+          <Button
             onClick={() => {
               setState(state => !state);
             }}
+            title="Toggle Transition"
           >
-            Button
-          </button>
+            Toggle Transition
+          </Button>
+          <br />
+          <br />
           <Transition
             isActive={state}
             type={select(
