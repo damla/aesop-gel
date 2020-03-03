752a153c72107ea826b5a2859f383ae12d92e61a
Beau Charman <beau.charman@gmail.com>
2020-03-04 09:50:30 +1100

    now sb deploy

--- deploy-storybook.js
+++ deploy-storybook.js
@@ -1,9 +1,5 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('storybook-static', {
-  dest: '.',
-  branch: 'gh-pages',
-  src: '**/*',
-  remote: 'origin',
   message: 'Auto-generated commit: Update Storybook',
 });
