564ebc58353e595b4d5ca904faf7676f55de8b31
Beau Charman <beau.charman@gmail.com>
2020-03-06 12:00:14 +1100

    Update useOnScreen.js

--- src/hooks/useOnScreen.js
+++ src/hooks/useOnScreen.js
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react';
 
+const hasIntersectionObserver =
+  'IntersectionObserver' in window ||
+  'IntersectionObserverEntry' in window ||
+  'intersectionRatio' in window.IntersectionObserverEntry.prototype;
+
 export const useOnScreen = (ref, rootMargin = '0px') => {
   const [isVisible, setVisible] = useState(false);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            setVisible(entry.isIntersecting);
-          }
-        });
-      },
-      {
-        rootMargin,
-      },
-    );
-    observer.observe(ref.current);
-    return () => observer.unobserve(ref.current);
+    if (!hasIntersectionObserver) {
+      setVisible(true);
+    } else {
+      const observer = new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              setVisible(entry.isIntersecting);
+            }
+          });
+        },
+        {
+          rootMargin,
+        },
+      );
+
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (!hasIntersectionObserver) {
+        setVisible(true);
+      }
+    };
   }, []);
 
   return isVisible;
