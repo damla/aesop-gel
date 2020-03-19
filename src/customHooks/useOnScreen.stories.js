cbd477fd6e7f1f538d08fe68cf06d73388ce4173
Beau Charman <beau.charman@gmail.com>
2020-03-20 08:15:51 +1100

    Adjust styles for Modal and Kit  List

--- src/customHooks/useOnScreen.stories.js
+++ src/customHooks/useOnScreen.stories.js
@@ -28,13 +28,15 @@ storiesOf('Hooks/useOnScreen', module).add('useOnScreen', () => {
           isVisible={isOnScreen}
           items={[
             {
-              term: 'Aroma',
               description: 'Fresh, woody, citrus',
+              id: 'Aroma',
+              term: 'Aroma',
             },
             {
-              term: 'Usage',
               description:
                 'Spray  two to three pumps throughout the immediate space and refresh as needed; the aroma will last for several hours.',
+              id: 'Usage',
+              term: 'Usage',
             },
           ]}
         />
