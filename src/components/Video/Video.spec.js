3f2cd85f2f3d5bebf060adae2f0b431569b61b30
Beau Charman <hello@ghbot.com>
2020-03-31 15:36:03 +1100

    update snapshots and video prop requirements

--- src/components/Video/Video.spec.js
+++ src/components/Video/Video.spec.js
@@ -20,6 +20,11 @@ describe('<Video />', () => {
           id="video"
           large="https://player.vimeo.com/external/300219772.hd.mp4?s=981a375b08836e6d99532afaefda0b9a78fbf404&amp;profile_id=174"
           medium="https://player.vimeo.com/external/300219772.hd.mp4?s=981a375b08836e6d99532afaefda0b9a78fbf404&amp;profile_id=174"
+          poster={{
+            copy: {
+              altText: 'How to Use Video for Tea Tree Leaf Facial Exfoliant',
+            },
+          }}
           small="https://player.vimeo.com/external/300219772.hd.mp4?s=981a375b08836e6d99532afaefda0b9a78fbf404&amp;profile_id=174"
         />,
       )
