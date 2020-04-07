234849774899b7461e908db259a7a0b8443c4527
Beau Charman <beau.charman@gmail.com>
2020-04-07 15:55:17 +1000

    update carousel styles

--- src/components/Carousel/components/Pagination/Pagination.js
+++ src/components/Carousel/components/Pagination/Pagination.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import cx from 'classnames';
+// import findIndex from 'lodash/findIndex';
 import PropTypes from 'prop-types';
 import styles from './Pagination.module.css';
 
-const Pagination = ({ dots, theme }) => (
-  <div className={styles.base}>
-    <ul className={cx(styles.list, styles[theme])}>{dots}</ul>
-  </div>
-);
+const Pagination = ({ dots, theme }) => {
+  // const progress = findIndex(dots, ['props.className', 'slick-active']);
+  // const width = `${100 / dots.length}%`;
+  // const positionLeft = `${(100 / dots.length) * progress}%`;
+
+  return (
+    <div className={styles.base}>
+      <ul className={cx(styles.list, styles[theme])}>{dots}</ul>
+    </div>
+  );
+};
 
 Pagination.propTypes = {
   dots: PropTypes.array,
