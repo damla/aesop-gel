20e8ad1eb8bf30687d012d04aae732acc690561b
Beau Charman <beau.charman@gmail.com>
2020-04-16 16:07:15 +1000

    use threshold in useOnScreen

--- src/customHooks/useOnScreen.stories.js
+++ src/customHooks/useOnScreen.stories.js
@@ -8,38 +8,42 @@ import Transition from '~/components/Transition';
 
 storiesOf('Hooks/useOnScreen', module).add('useOnScreen', () => {
   const ref = useRef();
-  const isOnScreen = useOnScreen(ref, '-50px');
+  const isOnScreen = useOnScreen(ref, 0.25);
 
   return (
     <div>
-      <div style={{ height: '140vh' }}>
+      <div style={{ height: '100vh' }}>
         <P>
           The <code>useOnScreen</code> custom hook takes a ref of a DOM Element
           and an offset, and returns a boolean of true when that referenced
           element is within the viewport.
         </P>
         <P>
-          Scroll down to see the next element reveal, with an offset of -50px.
+          Scroll down to see the next element reveal, with an offset threshold
+          of 25% (0.25).
         </P>
       </div>
-      <Transition isActive={isOnScreen} type="shiftInDown">
-        <KitList
-          isVisible={isOnScreen}
-          items={[
-            { content: 'Parsley Seed Facial Cleansing Oil, 200 mL', id: '1' },
-            {
-              content: 'Parsley Seed Anti-Oxidant Facial Toner, 100 mL',
-              id: '2',
-            },
-            { content: 'Parsley Seed Anti-Oxidant Serum, 100 mL', id: '3' },
-            {
-              content: 'Parsley Seed Anti-Oxidant Eye Cream, 10 mL',
-              id: '4',
-            },
-          ]}
-          ref={ref}
-        />
-      </Transition>
+      <div style={{ backgroundColor: '#4a4a4a', padding: '0 20px' }}>
+        <Transition isActive={isOnScreen} type="shiftInDown">
+          <KitList
+            isVisible={isOnScreen}
+            items={[
+              { content: 'Parsley Seed Facial Cleansing Oil, 200 mL', id: '1' },
+              {
+                content: 'Parsley Seed Anti-Oxidant Facial Toner, 100 mL',
+                id: '2',
+              },
+              { content: 'Parsley Seed Anti-Oxidant Serum, 100 mL', id: '3' },
+              {
+                content: 'Parsley Seed Anti-Oxidant Eye Cream, 10 mL',
+                id: '4',
+              },
+            ]}
+            ref={ref}
+            theme="light"
+          />
+        </Transition>
+      </div>
     </div>
   );
 });
