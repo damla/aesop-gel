1d14ff8747cadeb2862411d9afcb2837324a9112
Beau Charman <beau.charman@gmail.com>
2020-06-25 13:34:57 +1000

    feat: add regenerator runtime to jest tests

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-  ],
+  presets: [['@babel/preset-env'], '@babel/preset-react'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     [
