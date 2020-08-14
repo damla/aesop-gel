93d6e5b2fe6a1975dda2f12f3726ccf8fa0c0f88
Kevin Ho <kevin_ho87@yahoo.com.au>
2020-08-14 14:17:46 +1000

    feat: aes-1695: Implement ATC

--- src/components/ProductGridItem/ProductGridItem.fixture.js
+++ src/components/ProductGridItem/ProductGridItem.fixture.js
@@ -1,1 +1,64 @@
-export default {};
+export default {
+  variantOptions: [
+    {
+      size: '50 mL - In stock',
+      sku: 'ARD33',
+      price: '$26.45',
+      isInStock: true,
+      image: {
+        altText: 'alt-tag-ARD33',
+        sizes: {
+          large: './assets/images/Product/variant-one-large.png',
+          medium: './assets/images/Product/variant-one-medium.png',
+          small: './assets/images/Product/variant-one-small.png',
+        },
+      },
+    },
+    {
+      size: '100 mL - Not in stock',
+      sku: 'ARD32',
+      price: '$86.97',
+      isInStock: false,
+      image: {
+        altText: 'alt-tag-ARD32',
+        sizes: {
+          large: './assets/images/Product/variant-two-large.png',
+          medium: './assets/images/Product/variant-two-medium.png',
+          small: './assets/images/Product/variant-two-small.png',
+        },
+      },
+    },
+    {
+      size: '180 mL - In stock',
+      sku: 'ARD30',
+      price: '$96.97',
+      isInStock: true,
+      image: {
+        altText: 'alt-tag-ARD32',
+        sizes: {
+          large: './assets/images/Product/variant-two-large.png',
+          medium: './assets/images/Product/variant-two-medium.png',
+          small: './assets/images/Product/variant-two-small.png',
+        },
+      },
+    },
+    {
+      alternateAction: {
+        url: '/?searchMenu=open',
+        label: 'Visit Store - $89.45',
+      },
+      size: '500 mL - Alternate action',
+      sku: 'ARD31',
+      price: '$109.45',
+      isInStock: true,
+      image: {
+        altText: 'alt-tag-ARD31',
+        sizes: {
+          large: './assets/images/Product/variant-two-large.png',
+          medium: './assets/images/Product/variant-two-medium.png',
+          small: './assets/images/Product/variant-two-small.png',
+        },
+      },
+    },
+  ],
+};
--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+
+// import { useVariantSelectContext } from '~/contexts';
+import AddToCartButton from '~/components/AddToCartButton';
+
 import styles from './ProductGridItem.module.css';
 
-const ProductGridItem = ({ className }) => {
+const ProductGridItem = ({ className, copy, theme }) => {
+  // const {
+  //   selectedVariant,
+  //   onVariantChange,
+  //   variants,
+  // } = useVariantSelectContext();
+
   const classSet = cx(styles.base, className);
+
+  const ADD_TO_CART_BUTTON_DATA_TEST_REF = 'PRODUCT_GRID_ITEM_ADD_TO_CART_CTA';
 
   return (
     <div className={classSet}>
@@ -12,7 +24,16 @@ const ProductGridItem = ({ className }) => {
       <p>Image</p>
       <p>Product title</p>
       <p>radio buttons</p>
+
       <p>Button add to cart</p>
+      <AddToCartButton
+        className={styles.addToCartButton}
+        copy={copy.addToCart}
+        dataTestRef={ADD_TO_CART_BUTTON_DATA_TEST_REF}
+        isFullWidth={false}
+        theme={theme}
+      />
+
       <p>list items</p>
     </div>
   );
@@ -20,10 +41,32 @@ const ProductGridItem = ({ className }) => {
 
 ProductGridItem.propTypes = {
   className: PropTypes.string,
+  copy: PropTypes.shape({
+    addToCart: PropTypes.shape({
+      cartAction: PropTypes.string,
+      updateNotification: PropTypes.string,
+      outOfStock: PropTypes.shape({
+        label: PropTypes.string,
+        title: PropTypes.string,
+      }),
+    }),
+  }),
+  theme: PropTypes.oneOf(['dark', 'light']),
 };
 
 ProductGridItem.defaultProps = {
   className: undefined,
+  copy: {
+    addToCart: {
+      cartAction: undefined,
+      updateNotification: undefined,
+      outOfStock: {
+        label: undefined,
+        title: undefined,
+      },
+    },
+  },
+  theme: 'dark',
 };
 
 export default ProductGridItem;
