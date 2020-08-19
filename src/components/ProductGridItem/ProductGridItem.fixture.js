20d87201550f2f863a99ca7704e13ea1b166ab06
Kevin Ho <kevin_ho87@yahoo.com.au>
2020-08-19 15:00:59 +1000

    feat: aes-1695: stories update, update test

--- src/components/ProductGridItem/ProductGridItem.fixture.js
+++ src/components/ProductGridItem/ProductGridItem.fixture.js
@@ -1,5 +1,21 @@
 export default {
   info: '4 Sizes / From $ 26.45',
+  variantOptionsOneItem: [
+    {
+      size: '50 mL - In stock',
+      sku: 'ARD33',
+      price: '$26.45',
+      isInStock: true,
+      image: {
+        altText: 'alt-tag-ARD33',
+        sizes: {
+          large: './assets/images/Product/variant-one-large.png',
+          medium: './assets/images/Product/variant-one-medium.png',
+          small: './assets/images/Product/variant-one-small.png',
+        },
+      },
+    },
+  ],
   variantOptions: [
     {
       size: '50 mL - In stock',
@@ -36,24 +52,6 @@ export default {
       isInStock: true,
       image: {
         altText: 'alt-tag-ARD32',
-        sizes: {
-          large: './assets/images/Product/variant-two-large.png',
-          medium: './assets/images/Product/variant-two-medium.png',
-          small: './assets/images/Product/variant-two-small.png',
-        },
-      },
-    },
-    {
-      alternateAction: {
-        url: '/?searchMenu=open',
-        label: 'Visit Store - $89.45',
-      },
-      size: '500 mL - Alternate action',
-      sku: 'ARD31',
-      price: '$109.45',
-      isInStock: true,
-      image: {
-        altText: 'alt-tag-ARD31',
         sizes: {
           large: './assets/images/Product/variant-two-large.png',
           medium: './assets/images/Product/variant-two-medium.png',
