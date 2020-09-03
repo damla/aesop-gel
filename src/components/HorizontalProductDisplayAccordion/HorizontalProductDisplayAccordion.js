ecf893add4d631f5b3cf48e6d68a3391b27b1a30
Sam Morgan <sam@svmorgan.com>
2020-09-03 15:29:01 +1000

    Adding HorizontalProductDisplayAccordion and FullscreenHeroScroll

---
+++ src/components/HorizontalProductDisplayAccordion/HorizontalProductDisplayAccordion.js
@@ -0,0 +1,220 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import {
+  ProductDetailContextProvider,
+  VariantSelectContextProvider,
+} from '~/contexts';
+import AccordionProduct from './components/AccordionProduct';
+import styles from './HorizontalProductDisplayAccordion.module.css';
+import { ascertainIsSmallOnlyViewport } from '~/utils/viewports';
+import useWindowHasResized from '~/customHooks/useWindowHasResized';
+
+const HorizontalProductDisplayAccordion = ({ id, products }) => {
+  const [accordionProducts, toggleAccordionProducts] = useState(products);
+  const [accordionActive, toggleAccordionActiveState] = useState(false);
+  let isMobile = ascertainIsSmallOnlyViewport();
+
+  const toggleAccordion = (index, opening) => {
+    toggleAccordionProducts(
+      accordionProducts.map((product, i) => {
+        if (isMobile) {
+          if (opening) {
+            if (i === index) {
+              product.expanded = true;
+            }
+          } else {
+            if (i === index) {
+              product.expanded = false;
+            }
+          }
+        } else {
+          if (opening) {
+            if (i === index) {
+              product.expanded = true;
+              product.compressed = false;
+            } else {
+              product.expanded = false;
+              product.compressed = true;
+            }
+            toggleAccordionActiveState(true);
+          } else {
+            if (i === index) {
+              product.expanded = false;
+              product.compressed = false;
+            } else {
+              product.expanded = false;
+              product.compressed = false;
+            }
+            toggleAccordionActiveState(false);
+          }
+        }
+        return product;
+      }),
+    );
+  };
+
+  const resetAccordion = () => {
+    toggleAccordionProducts(
+      accordionProducts.map((product, i) => {
+        product.expanded = false;
+        product.compressed = false;
+        console.log('reset');
+        return product;
+      }),
+    );
+  };
+
+  window.addEventListener('resize', () => {
+    if (ascertainIsSmallOnlyViewport() === false && isMobile === true) {
+      accordionProducts.map(product => {
+        product.expanded = false;
+      });
+      isMobile = ascertainIsSmallOnlyViewport();
+    }
+  });
+
+  useWindowHasResized();
+
+  return (
+    <div
+      className={
+        (accordionActive ? 'accordion-active ' : '') +
+        cx(styles.accordionProductWrap)
+      }
+      id={id}
+    >
+      {accordionProducts.map(({ ...product }, index) => {
+        return (
+          <ProductDetailContextProvider
+            key={index}
+            /**
+              src/contexts/ProductDetail.context.js
+              expects {
+                description,
+                id,
+                variantOptions,
+                cartDisclaimer,
+                definitionList,
+                ingredients,
+                keyIngredient,
+                productName,
+              }
+            */
+            product={{
+              description: product.openState.eyebrow,
+              productName: product.openState.title,
+            }}
+          >
+            <VariantSelectContextProvider variants={product.variantOptions}>
+              <AccordionProduct
+                index={index}
+                resetAccordion={resetAccordion}
+                toggleAccordion={toggleAccordion}
+                {...product}
+              />
+            </VariantSelectContextProvider>
+          </ProductDetailContextProvider>
+        );
+      })}
+    </div>
+  );
+};
+
+HorizontalProductDisplayAccordion.propTypes = {
+  className: PropTypes.string,
+  id: PropTypes.string,
+  openIndex: PropTypes.string,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      closedState: PropTypes.shape({
+        background: PropTypes.oneOf(['Colour', 'Image', 'Video']),
+        backgroundColour: PropTypes.string,
+        backgroundImage: PropTypes.object,
+        backgroundVideo: PropTypes.object,
+        copy: PropTypes.node,
+        eyebrow: PropTypes.string,
+        foregroundImage: PropTypes.object,
+        id: PropTypes.string,
+        openButtonText: PropTypes.string,
+        theme: PropTypes.string,
+        title: PropTypes.string,
+      }),
+      compressed: PropTypes.bool,
+      expanded: PropTypes.bool,
+      id: PropTypes.string,
+      index: PropTypes.number,
+      openState: PropTypes.shape({
+        background: PropTypes.oneOf(['Colour', 'Image', 'Video']),
+        backgroundColour: PropTypes.string,
+        backgroundImage: PropTypes.object,
+        backgroundVideo: PropTypes.object,
+        closeButtonText: PropTypes.string,
+        copy: PropTypes.node,
+        eyebrow: PropTypes.string,
+        foregroundImage: PropTypes.object,
+        theme: PropTypes.string,
+        title: PropTypes.string,
+      }),
+      toggleAccordion: PropTypes.func,
+      variantOptions: PropTypes.arrayOf(
+        PropTypes.shape({
+          isInStock: PropTypes.bool,
+          price: PropTypes.string,
+          size: PropTypes.string,
+          sku: PropTypes.string,
+        }),
+      ),
+    }),
+  ),
+};
+
+// HorizontalProductDisplayAccordion.propTypes = {
+//  className: PropTypes.string,
+//  id: PropTypes.string,
+//  openIndex: PropTypes.string,
+//  products: PropTypes.array,
+// };
+
+HorizontalProductDisplayAccordion.defaultProps = {
+  className: undefined,
+  id: undefined,
+  openIndex: null,
+  products: {
+    closedState: {
+      background: 'Image',
+      backgroundColour: undefined,
+      backgroundImage: undefined,
+      backgroundVideo: undefined,
+      closeButtonText: undefined,
+      copy: undefined,
+      eyebrow: undefined,
+      foregroundImage: undefined,
+      id: undefined,
+      theme: undefined,
+      title: undefined,
+    },
+    compressed: undefined,
+    expanded: undefined,
+    id: undefined,
+    index: undefined,
+    openState: {
+      background: 'Colour',
+      backgroundColour: undefined,
+      backgroundImage: undefined,
+      backgroundVideo: undefined,
+      copy: undefined,
+      eyebrow: undefined,
+      foregroundImage: undefined,
+      openButtonText: undefined,
+      theme: undefined,
+      title: undefined,
+    },
+    toggleAccordion: undefined,
+    variantOptions: {
+      isInStock: true,
+    },
+  },
+};
+
+export default HorizontalProductDisplayAccordion;
---
+++ src/components/HorizontalProductDisplayAccordion/components/AccordionProduct/AccordionProduct.spec.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import CarouselFixture from '../../Carousel.fixture';
+import Slide from './Slide';
+import AccordionProduct from './AccordionProduct';
+
+configure({ adapter: new Adapter() });
+
+describe('<Carousel.Slide />', () => {
+  it('should be defined', () => {
+    expect(Slide).toBeDefined();
+  });
+
+  it('renders base component correctly', () => {
+    const tree = renderer
+      .create(
+        <AccordionProduct
+          description={CarouselFixture.slides[0].description}
+          heading={CarouselFixture.slides[0].heading}
+          image={CarouselFixture.slides[0].image}
+        />,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders a slide without a descritption correctly', () => {
+    const tree = renderer
+      .create(
+        <AccordionProduct
+          heading={CarouselFixture.slides[0].heading}
+          image={CarouselFixture.slides[0].image}
+        />,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders a slide without a heading correctly', () => {
+    const tree = renderer
+      .create(
+        <AccordionProduct
+          description={CarouselFixture.slides[0].description}
+          image={CarouselFixture.slides[0].image}
+        />,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
