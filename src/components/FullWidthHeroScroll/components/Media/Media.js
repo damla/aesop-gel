ab8ad44372d02e655426bb240c143bdfd36f90ed
Sam Morgan <sam@svmorgan.com>
2020-09-03 15:29:01 +1000

    Adding HorizontalProductDisplayAccordion and FullscreenHeroScroll

---
+++ src/components/FullWidthHeroScroll/components/Media/Media.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Figure from '~/components/Figure';
+import Hyperlink from '~/components/Hyperlink';
+import styles from './Media.module.css';
+
+const Media = ({
+  className,
+  foregroundImage,
+  foregroundImageLink,
+  hasFullWidthImage,
+  isHero,
+  media,
+}) => {
+  const classSet = cx(
+    className,
+    styles.base,
+    { [styles.hero]: isHero },
+    { [styles.hasFullWidthImage]: hasFullWidthImage },
+  );
+
+  return (
+    <div className={classSet}>
+      <Figure className={styles.media}>{media}</Figure>
+      {foregroundImage && (
+        <Figure className={styles.foregroundImage}>
+          {foregroundImageLink ? (
+            <Hyperlink
+              title={foregroundImageLink.title}
+              url={foregroundImageLink.url}
+            >
+              {foregroundImage}
+            </Hyperlink>
+          ) : (
+            foregroundImage
+          )}
+        </Figure>
+      )}
+    </div>
+  );
+};
+
+Media.propTypes = {
+  className: PropTypes.string,
+  foregroundImage: PropTypes.element,
+  foregroundImageLink: PropTypes.shape({
+    url: PropTypes.string,
+    title: PropTypes,
+  }),
+  hasFullWidthImage: PropTypes.bool,
+  isHero: PropTypes.bool,
+  media: PropTypes.element.isRequired,
+};
+
+Media.defaultProps = {
+  className: undefined,
+  foregroundImage: undefined,
+  foregroundImageLink: undefined,
+  hasFullWidthImage: false,
+  isHero: false,
+  media: undefined,
+};
+
+export default Media;
