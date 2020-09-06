79fc2d5aa41fe5242081f126d17522920c9df56c
Sam Morgan <sam@svmorgan.com>
2020-09-07 09:33:01 +1000

    feat(aes-49-new): cleaned up code as requested in pull request

---
+++ src/components/FullWidthHeroScroll/components/FullscreenVideo/FullscreenVideo.js
@@ -0,0 +1,83 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styles from './FullscreenVideo.module.css';
+import { getViewportForWidth } from '~/utils/viewports';
+
+const FullscreenVideo = ({ large, medium, small }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    let viewportSize = getViewportForWidth(window.innerWidth);
+
+    const bgVideoInit = ref => {
+      const vid = ref.current;
+      let videoUrl = large;
+      if (viewportSize === 'small') {
+        videoUrl = small;
+      }
+      if (viewportSize === 'medium') {
+        videoUrl = medium;
+      }
+      if (videoUrl) {
+        vid.src = videoUrl;
+        // objectFitVideos();
+
+        vid.oncanplay = () => {
+          const promise = vid.play();
+          if (promise !== undefined) {
+            promise
+              .then(() => {
+                vid.parentElement.classList.add('autoplay-enabled');
+                // objectFitVideos();
+              })
+              .catch(() => {
+                vid.parentElement.classList.add('autoplay-disabled');
+              });
+          }
+        };
+        vid.addEventListener(
+          'loadedmetadata',
+          () => {
+            const ratio = (vid.videoHeight / vid.videoWidth) * 100;
+            vid.parentElement.dataset.ratio = ratio;
+          },
+          false,
+        );
+      }
+    };
+    bgVideoInit(ref);
+    window.addEventListener('resize', () => {
+      const viewportSizeNew = getViewportForWidth(window.innerWidth);
+      if (viewportSizeNew !== viewportSize) {
+        viewportSize = viewportSizeNew;
+        bgVideoInit(ref);
+      }
+    });
+  }, [ref]);
+
+  return (
+    <video
+      autoPlay={true}
+      className={cx(styles.fullscreenVideo)}
+      loop={true}
+      muted={true}
+      playsInline={true}
+      ref={ref}
+    />
+  );
+};
+
+FullscreenVideo.propTypes = {
+  large: PropTypes.string,
+  medium: PropTypes.string,
+  small: PropTypes.string,
+};
+
+FullscreenVideo.defaultProps = {
+  large: undefined,
+  medium: undefined,
+  small: undefined,
+};
+
+export default FullscreenVideo;
