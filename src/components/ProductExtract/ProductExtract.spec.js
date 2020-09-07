b18cfa00a529eb8b405c0ea521f52c07e9523c16
Hassan Raza <m-h-r300@hotmail.com>
2020-09-07 22:26:36 +1000

    feat: add ProductExtract css, fixture, test and story file

---
+++ src/components/ProductExtract/ProductExtract.spec.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+import ProductExtract from './ProductExtract';
+import ProductExtractFixture from './ProductExtract.fixture';
+
+configure({ adapter: new Adapter() });
+
+describe('<ProductExtract />', () => {
+  it('should be defined', () => {
+    expect(ProductExtract).toBeDefined();
+  });
+
+  it('renders base component correctly', () => {
+    const tree = renderer
+      .create(
+        <ProductExtract
+          dataTestRef={ProductExtractFixture.dataTestRef}
+          product={ProductExtractFixture.product}
+        />
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('returns null product prop is not a populated object', () => {
+    const component = shallow(
+      <ProductExtract
+        dataTestRef={ProductExtractFixture.dataTestRef}
+        product={{}}
+      />
+    );
+    expect(component.type()).toEqual(null);
+  });
+});
