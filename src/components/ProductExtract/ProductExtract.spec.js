10e731e80b486ede9b0a2e00b481dc0428f583e2
Hassan Raza <m-h-r300@hotmail.com>
2020-09-10 12:37:59 +1000

    feat: fix fixture filename and import statements

--- src/components/ProductExtract/ProductExtract.spec.js
+++ src/components/ProductExtract/ProductExtract.spec.js
@@ -3,7 +3,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import ProductExtract from './ProductExtract';
-import ProductExtractFixture from './ProductExtractfixture';
+import ProductExtractFixture from './ProductExtract.fixture';
 
 configure({ adapter: new Adapter() });
 
