06957fb88d9112e371bc7b88073793d8c4ade9bb
zack76 <szhu76@gmail.com>
2020-09-16 15:19:33 +1000

    feat: new component - FooterBlock

---
+++ src/components/FooterBlock/FooterBlock.fixture.js
@@ -0,0 +1,42 @@
+export default {
+  actionLabel: 'Learn More',
+  block: {
+    title: 'Sustainability',
+    copy:
+      'Our approach to sustainability is guided by where we can have the greatest impact. We have ceritifications from Crulety Free International as well as B Corp Certifications',
+  },
+  blockNoLink: {
+    title: 'Acknowledgment of Country ',
+    copy:
+      'Aesop acknowledges the Traditional Owners of the lands on which we live and work, and pays deepest respects to Elders past, present and emerging. We also acknowledge that sovereignty of these lands has never been ceded.',
+  },
+  navigation: {
+    title: 'Orders & Support',
+    links: [
+      {
+        text: 'Contact us',
+        id: 'contact-us',
+        url: 'https://www.aesop.com/au/r/contact-us',
+        openInANewWindow: true,
+      },
+      {
+        text: 'Delivery and returns',
+        id: 'delivery-and-returns',
+        url: 'https://www.aesop.com/au/r/shipping/',
+        openInANewWindow: true,
+      },
+      {
+        text: 'Click And Collect',
+        id: 'click-and-collect',
+        url: 'https://www.aesop.com/au/r/contact-us',
+        openInANewWindow: true,
+      },
+      {
+        text: 'Track Your Orders',
+        id: 'track-your-orders',
+        url: 'https://www.aesop.com/au/r/shipping/',
+        openInANewWindow: false,
+      },
+    ],
+  },
+};
