d43200cef916aa3b4f5b8bb1d7a574a542c6818a
zack76 <szhu76@gmail.com>
2020-09-18 11:24:05 +1000

    fix: improve accessibility

--- src/components/FooterBlock/FooterBlock.fixture.js
+++ src/components/FooterBlock/FooterBlock.fixture.js
@@ -1,17 +1,17 @@
 export default {
   actionLabel: 'Learn More',
   block: {
-    title: 'Sustainability',
+    heading: 'Sustainability',
     copy:
       'Our approach to sustainability is guided by where we can have the greatest impact. We have ceritifications from Crulety Free International as well as B Corp Certifications',
   },
   blockNoLink: {
-    title: 'Acknowledgment of Country ',
+    heading: 'Acknowledgment of Country ',
     copy:
       'Aesop acknowledges the Traditional Owners of the lands on which we live and work, and pays deepest respects to Elders past, present and emerging. We also acknowledge that sovereignty of these lands has never been ceded.',
   },
   navigation: {
-    title: 'Orders & Support',
+    heading: 'Orders & Support',
     links: [
       {
         text: 'Contact us',
