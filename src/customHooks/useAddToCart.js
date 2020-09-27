0c58af2c81a88129be66ebbf45b8b794263fc595
Beau Charman <beau.charman@gmail.com>
2020-09-28 09:48:57 +1000

    feat: aes-49 add correct error message payload

--- src/customHooks/useAddToCart.js
+++ src/customHooks/useAddToCart.js
@@ -44,11 +44,12 @@ function reducer(state, action) {
 
 const useAddToCart = onClick => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { isLoading, hasError, isUpdateSuccessful } = state;
+  const { errorMessage, hasError, isLoading, isUpdateSuccessful } = state;
 
   return {
     actionTypes: ADD_TO_CART_ACTION_TYPES,
     dispatch,
+    errorMessage,
     hasError,
     isLoading,
     isUpdateSuccessful,
