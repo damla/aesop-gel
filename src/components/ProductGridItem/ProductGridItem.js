d09991092c225088f7ebfd13182e136d6a85a229
Kevin Ho <kevin_ho87@yahoo.com.au>
2020-10-14 18:08:35 +1100

    feat: gift-guide: prod grid item forwardRef

--- src/components/ProductGridItem/ProductGridItem.js
+++ src/components/ProductGridItem/ProductGridItem.js
@@ -18,117 +18,119 @@ import { P } from '~/components/Paragraph';
 
 import styles from './ProductGridItem.module.css';
 
-const ProductGridItem = ({ className, copy, cta, id, info, theme, url }) => {
-  const imageRef = useRef();
-  const {
-    selectedVariant,
-    onVariantChange,
-    variants,
-  } = useVariantSelectContext();
-  const { productDetail } = useProductDetailContext();
+const ProductGridItem = React.forwardRef(
+  ({ className, copy, cta, id, info, theme, url }, ref) => {
+    const imageRef = useRef();
+    const {
+      selectedVariant,
+      onVariantChange,
+      variants,
+    } = useVariantSelectContext();
+    const { productDetail } = useProductDetailContext();
 
-  const hasOneVariant = variants?.length === 1;
-  const isBundle = variants?.length === 0;
+    const hasOneVariant = variants?.length === 1;
+    const isBundle = variants?.length === 0;
 
-  const [currentImage, isImageActive] = useImageTransition(
-    selectedVariant?.image || productDetail.image,
-    imageRef,
-  );
+    const [currentImage, isImageActive] = useImageTransition(
+      selectedVariant?.image || productDetail.image,
+      imageRef,
+    );
 
-  if (!productDetail) return null;
+    if (!productDetail) return null;
 
-  const { definitionList, productName, sku } = productDetail;
+    const { definitionList, productName, sku } = productDetail;
 
-  const variantRadioOptions = getVariantRadioOptions(variants);
-  const classSet = cx(styles.base, styles[theme], className);
+    const variantRadioOptions = getVariantRadioOptions(variants);
+    const classSet = cx(styles.base, styles[theme], className);
 
-  const classInfoHolderSet = cx(styles.infoHolder, {
-    [styles.hasOneVariant]: hasOneVariant || isBundle,
-    [styles.hasManyVariants]: !hasOneVariant,
-  });
+    const classInfoHolderSet = cx(styles.infoHolder, {
+      [styles.hasOneVariant]: hasOneVariant || isBundle,
+      [styles.hasManyVariants]: !hasOneVariant,
+    });
 
-  const classCtaLinkSet = cx(styles.addToCartButton, styles.ctaLink);
+    const classCtaLinkSet = cx(styles.addToCartButton, styles.ctaLink);
 
-  const RADIO_GROUP_NAME = sku;
-  const RADIO_GROUP_DATA_TEST_REF = 'PRODUCT_GRID_ITEM_VARIANT_SELECT';
+    const RADIO_GROUP_NAME = sku;
+    const RADIO_GROUP_DATA_TEST_REF = 'PRODUCT_GRID_ITEM_VARIANT_SELECT';
 
-  return (
-    <div className={classSet} id={id}>
-      <Transition isActive={isImageActive} name="fade">
-        <Hyperlink className={styles.imageLink} url={url}>
-          <Image
-            altText={currentImage.altText}
-            className={styles.image}
-            large={currentImage.sizes?.large}
-            medium={currentImage.sizes?.medium}
-            ref={imageRef}
-            small={currentImage.sizes?.small}
-          />
-        </Hyperlink>
-      </Transition>
+    return (
+      <div className={classSet} id={id} ref={ref}>
+        <Transition isActive={isImageActive} name="fade">
+          <Hyperlink className={styles.imageLink} url={url}>
+            <Image
+              altText={currentImage.altText}
+              className={styles.image}
+              large={currentImage.sizes?.large}
+              medium={currentImage.sizes?.medium}
+              ref={imageRef}
+              small={currentImage.sizes?.small}
+            />
+          </Hyperlink>
+        </Transition>
 
-      <Heading
-        className={styles.productName}
-        hasMediumWeightFont={true}
-        level={HEADING.LEVEL.FIVE}
-        size={HEADING.SIZE.X_X_SMALL}
-        theme={theme}
-      >
-        <Hyperlink className={styles.productNameLink} url={url}>
-          {productName}
-        </Hyperlink>
-      </Heading>
+        <Heading
+          className={styles.productName}
+          hasMediumWeightFont={true}
+          level={HEADING.LEVEL.FIVE}
+          size={HEADING.SIZE.X_X_SMALL}
+          theme={theme}
+        >
+          <Hyperlink className={styles.productNameLink} url={url}>
+            {productName}
+          </Hyperlink>
+        </Heading>
 
-      <div className={styles.infoVariantHolder}>
-        <div className={classInfoHolderSet}>
-          <P className={styles.info} theme={theme}>
-            {info}
-          </P>
+        <div className={styles.infoVariantHolder}>
+          <div className={classInfoHolderSet}>
+            <P className={styles.info} theme={theme}>
+              {info}
+            </P>
+          </div>
+
+          {!hasOneVariant && !isBundle && (
+            <RadioGroup
+              className={styles.variants}
+              dataTestRef={RADIO_GROUP_DATA_TEST_REF}
+              name={RADIO_GROUP_NAME}
+              onChange={e => onVariantChange(e, variants)}
+              options={variantRadioOptions}
+              theme={theme}
+              value={selectedVariant.sku}
+            />
+          )}
         </div>
 
-        {!hasOneVariant && !isBundle && (
-          <RadioGroup
-            className={styles.variants}
-            dataTestRef={RADIO_GROUP_DATA_TEST_REF}
-            name={RADIO_GROUP_NAME}
-            onChange={e => onVariantChange(e, variants)}
-            options={variantRadioOptions}
+        {!isBundle && (
+          <AddToCartButton
+            className={styles.addToCartButton}
+            copy={copy.addToCart}
+            isFullWidth={true}
             theme={theme}
-            value={selectedVariant.sku}
           />
         )}
-      </div>
 
-      {!isBundle && (
-        <AddToCartButton
-          className={styles.addToCartButton}
-          copy={copy.addToCart}
-          isFullWidth={true}
+        {cta && isBundle && (
+          <Hyperlink
+            className={classCtaLinkSet}
+            isAlternate={false}
+            theme={theme}
+            title={cta?.text}
+            url={cta?.url}
+          >
+            {cta?.text}
+          </Hyperlink>
+        )}
+
+        <DefinitionList
+          className={styles.definitionList}
+          hasBottomBorder={true}
+          items={definitionList}
           theme={theme}
         />
-      )}
-
-      {cta && isBundle && (
-        <Hyperlink
-          className={classCtaLinkSet}
-          isAlternate={false}
-          theme={theme}
-          title={cta?.text}
-          url={cta?.url}
-        >
-          {cta?.text}
-        </Hyperlink>
-      )}
-
-      <DefinitionList
-        className={styles.definitionList}
-        hasBottomBorder={true}
-        items={definitionList}
-        theme={theme}
-      />
-    </div>
-  );
-};
+      </div>
+    );
+  },
+);
 
 ProductGridItem.propTypes = {
   className: PropTypes.string,
