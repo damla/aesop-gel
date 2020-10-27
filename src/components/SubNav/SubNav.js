3457ca8af0d3f4cef429d4b9202550fb5da6d356
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2020-10-28 08:43:48 +1100

    feat(subnav): pass ref in

--- src/components/SubNav/SubNav.js
+++ src/components/SubNav/SubNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { useThemeContext } from '~/contexts';
@@ -10,64 +10,61 @@ import Select from '~/components/Select';
 import List from '~/components/List';
 import styles from './SubNav.module.css';
 
-const SubNav = ({
-  className,
-  heading,
-  headingClassName,
-  id,
-  isSelect,
-  links,
-  theme,
-}) => {
-  useWindowHasResized();
+const SubNav = forwardRef(
+  (
+    { className, heading, headingClassName, id, isSelect, links, theme },
+    ref,
+  ) => {
+    useWindowHasResized();
 
-  const currentTheme = useThemeContext(theme, 'dark');
-  const classSet = cx(
-    styles.base,
-    styles[currentTheme],
-    { [styles.select]: isSelect },
-    className,
-  );
-  const isSmallOrMediumViewport = ascertainIsSmallOrMediumOnlyViewport();
-  const onChange = event => {
-    window.location.href = event.target.value;
-  };
+    const currentTheme = useThemeContext(theme, 'dark');
+    const classSet = cx(
+      styles.base,
+      styles[currentTheme],
+      { [styles.select]: isSelect },
+      className,
+    );
+    const isSmallOrMediumViewport = ascertainIsSmallOrMediumOnlyViewport();
+    const onChange = event => {
+      window.location.href = event.target.value;
+    };
 
-  return (
-    <nav className={classSet}>
-      {heading && (
-        <Heading
-          className={cx(styles.heading, headingClassName)}
-          level="3"
-          noMargin={true}
-          size="small"
-          theme={currentTheme}
-        >
-          {heading}
-        </Heading>
-      )}
-      {isSelect && isSmallOrMediumViewport ? (
-        <Select
-          isBlock={isSelect}
-          name={id}
-          onChange={onChange}
-          options={links.map(({ children, id, url }) => ({
-            id,
-            label: children,
-            value: url,
-          }))}
-          theme={currentTheme}
-        />
-      ) : (
-        <List
-          items={getLinkItems(links, currentTheme)}
-          listItemClassName={styles.item}
-          theme={currentTheme}
-        />
-      )}
-    </nav>
-  );
-};
+    return (
+      <nav className={classSet} ref={ref}>
+        {heading && (
+          <Heading
+            className={cx(styles.heading, headingClassName)}
+            level="3"
+            noMargin={true}
+            size="small"
+            theme={currentTheme}
+          >
+            {heading}
+          </Heading>
+        )}
+        {isSelect && isSmallOrMediumViewport ? (
+          <Select
+            isBlock={isSelect}
+            name={id}
+            onChange={onChange}
+            options={links.map(({ children, id, url }) => ({
+              id,
+              label: children,
+              value: url,
+            }))}
+            theme={currentTheme}
+          />
+        ) : (
+          <List
+            items={getLinkItems(links, currentTheme)}
+            listItemClassName={styles.item}
+            theme={currentTheme}
+          />
+        )}
+      </nav>
+    );
+  },
+);
 
 export function getLinkItems(links, theme) {
   return links.map(({ children, id, hasTargetInNewWindow, style, url }) => ({
